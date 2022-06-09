5e5d0c44bcafbaab623c0b8199e60150ec9a689a
Zakk <muhammadzakaria616@gmail.com>
2022-06-09 09:52:39 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function () {
+  $("#chat-circle").click(function () {
+    $("#chat-circle").toggle("scale");
+    $(".chat-box").toggle("scale");
+  });
+
+  $(".chat-box-toggle").click(function () {
+    $("#chat-circle").toggle("scale");
+    $(".chat-box").toggle("scale");
+  });
+});
